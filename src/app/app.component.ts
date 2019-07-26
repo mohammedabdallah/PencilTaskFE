2a32a32ac18c9b5c31a73e88697eb985b52e5a47
mohamad.abdallah <mohamad.abdallah@robustastudio.com>
2019-07-26 18:49:21 +0200

    add store method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,33 @@ import { FormGroup, FormControl, FormArray, NgForm } from '@angular/forms';
 @Injectable()
 export class AppComponent implements OnInit {
     title = 'Pencil Task FE';
+    private productForm: FormGroup;
     products: any;
 
     constructor(private http: HttpClient) {
         this.getProducts();
+    }
+    ngOnInit() {
+        this.productForm = new FormGroup({
+            'name': new FormControl(),
+            'code': new FormControl(),
+            'price': new FormControl(),
+            'description': new FormControl()
+        });
     }
     //get all products
     getProducts() {
         return this.http.get('http://penciltask.wedev/api/v1/products').subscribe(products => {
             this.products = products.Allproducts;
         });
+    }
+    storeProduct(productForm: NgForm) {
+    	this.http.post('http://penciltask.wedev/api/v1/products',productForm.value).subscribe(res => {
+    		 //call get products to update table
+             this.getProducts();
+             //reset form after insert product
+             productForm.reset();
+    	});
     }
     //delete products
     deleteProduct(id) {
@@ -29,7 +46,6 @@ export class AppComponent implements OnInit {
     			this.getProducts();
     		});
   		}
-    	
     }
     
 }
