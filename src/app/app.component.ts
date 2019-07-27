7729e787a141f960e2b4a57d91eed521c4e14f0f
mohamad.abdallah <mohamad.abdallah@robustastudio.com>
2019-07-28 01:06:28 +0200

    add readme and repeatable fields

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { FormGroup, FormControl, FormArray, NgForm, Validators } from '@angular/forms';
+import { FormGroup, FormControl, FormArray, NgForm, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
@@ -12,37 +12,58 @@ import { FormGroup, FormControl, FormArray, NgForm, Validators } from '@angular/
 export class AppComponent implements OnInit {
     title = 'Pencil Task FE';
     private productForm: FormGroup;
-    products: any;
-    imagePreview: string;
-    productImage: any;
-    constructor(private http: HttpClient) {
+    public products: any;
+    public product: any;
+    public imagePreview: any;
+    public productImage: any;
+    public customfields: FormArray;
+
+    // returns all form groups under contacts
+    get contactFormGroup() {
+        return this.productForm.get('customfields') as FormArray;
+    }
+
+    constructor(private http: HttpClient,private fb: FormBuilder) {
         this.getProducts();
     }
     ngOnInit() {
         this.productForm = new FormGroup({
-            name: new FormControl(null,{ validators: [Validators.required] }),
-            code: new FormControl(null,{ validators: [Validators.required] }),
-            price: new FormControl(null,{ validators: [Validators.required] }),
-            description: new FormControl(null,{ validators: [Validators.required] }),
-            product_image: new FormControl(null,{ validators: [Validators.required] })
+            'name': new FormControl(null,{ validators: [Validators.required] }),
+            'code': new FormControl(null,{ validators: [Validators.required] }),
+            'price': new FormControl(null,{ validators: [Validators.required] }),
+            'description': new FormControl(null,{ validators: [Validators.required] }),
+            'product_image': new FormControl(null,{ validators: [Validators.required] }),
+            'customfields': this.fb.array([
+    			this.fb.control([this.createCustomField()])
+  			])
         });
+        this.customfields = this.productForm.get('customfields') as FormArray;
+       
     }
+    // contact formgroup
+  createCustomField(): FormGroup {
+    return this.fb.group({
+      cvalue: new FormControl(null,{ validators: [Validators.required] }),
+      cdescription: new FormControl(null,{ validators: [Validators.required] })
+    });
+  }
+  
     //get all products
     getProducts() {
         return this.http.get('http://penciltask.wedev/api/v1/products').subscribe(result => {
-            this.products = result.Allproducts;
+            this.products = result;
         });
     }
     //create new product
     storeProduct(productForm: NgForm) {
-    	// if(this.productForm.invalid)
-    	// 	return;
+        console.log(this.customfields.value);
     	const postData = new FormData();
     	postData.append('name',this.productForm.value.name);
     	postData.append('code',this.productForm.value.code);
     	postData.append('price',this.productForm.value.price);
     	postData.append('description',this.productForm.value.description);
     	postData.append("product_image", this.productImage);
+    	postData.append("customfields",this.customfields.value);
     	this.http.post('http://penciltask.wedev/api/v1/products',postData).subscribe(res => {
     		 //call get products to update table
              this.getProducts();
@@ -54,6 +75,12 @@ export class AppComponent implements OnInit {
     }
     //update product
     updateProduct(id,productForm: NgForm) {
+    	const postData = new FormData();
+    	postData.append('name',this.productForm.value.name);
+    	postData.append('code',this.productForm.value.code);
+    	postData.append('price',this.productForm.value.price);
+    	postData.append('description',this.productForm.value.description);
+    	postData.append("product_image", this.productImage);
     	this.http.put('http://penciltask.wedev/api/v1/products/' + id, this.productForm.value).subscribe(res => {
             this.getProducts();
         }, err => {
@@ -63,13 +90,14 @@ export class AppComponent implements OnInit {
     }
     showProduct(id) {
     	return this.http.get('http://penciltask.wedev/api/v1/products/' + id).subscribe(product => {
-            this.product = product.product;
+            this.product = product;
             this.productForm.patchValue({ 
                 id: this.product.id,
                 name: this.product.name,
                 price: this.product.price,
                 code: this.product.code,
                 description: this.product.description,
+                product_image: this.product.image_path
             });
         });
     }
@@ -93,5 +121,8 @@ export class AppComponent implements OnInit {
       this.imagePreview = reader.result;
     };
     reader.readAsDataURL(file);
+    }
+    addCustomField() {
+    	this.customfields.push(this.createCustomField());
     }
 }
